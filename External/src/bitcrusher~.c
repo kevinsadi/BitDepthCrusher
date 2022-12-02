76c6ce2ce2e35d850a0ee2f9278468b68539fd8d
Kevin <ksadi3@gatech.edu>
2022-12-02 13:55:38 -0500

    compiles, currently only pass through signal

--- External/src/bitcrusher~.c
+++ External/src/bitcrusher~.c
@@ -16,7 +16,7 @@ static t_class *bitcrush_tilde_class;
 typedef struct _bitcrush_tilde {
   t_object x_obj;
 
-  t_int bd; // there is a chance specifying an int inlet will cause mayhem
+  t_float bd;
 
   t_float f;
   t_inlet *x_in2;
@@ -35,18 +35,43 @@ typedef struct _bitcrush_tilde {
  */
 t_int *bitcrush_tilde_perform(t_int *w)
 {
+    t_bitcrush_tilde *x = (t_bitcrush_tilde *)(w[1]);
+    t_sample    *in1 =      (t_sample *)(w[2]);
+    t_sample    *out =      (t_sample *)(w[3]);
+    int            n =             (int)(w[4]);
 
+    while (n--) *out++ = (*in1++);
+
+    return (w+5);
 }
 
+void bitcrush_tilde_dsp(t_bitcrush_tilde *x, t_signal **sp)
+{
+  dsp_add(bitcrush_tilde_perform, 4, x,
+          sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_n);
+}
+
+void bitcrush_tilde_free(t_bitcrush_tilde *x)
+{
+  inlet_free(x->x_in2);
+  outlet_free(x->x_out);
+}
 
 /**
  * this is the "constructor" of the class
  * this method is called whenever a new object of this class is created
  * the name of this function is arbitrary and is registered to Pd in the
- * helloworld_setup() routine
+ * bitcrush_tilde_setup() routine
  */
-void *bitcrush_tilde_new(void)
+void *bitcrush_tilde_new(t_floatarg f)
 {
+    t_bitcrush_tilde *x = (t_bitcrush_tilde *)pd_new(bitcrush_tilde_class);
+
+    x->bd = f;
+
+    x->x_in2 = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
+
+    x->x_out = outlet_new(&x->x_obj, &s_signal);
 
   /*
    * return the pointer to the class - this is mandatory
@@ -61,16 +86,17 @@ void *bitcrush_tilde_new(void)
  * within a single-object external the name of this function is special
  */
 void bitcrush_tilde_setup(void) {
-  /* create a new class */
-  bitcrush_tilde_class = class_new(gensym("bitcrush~"),            /* the object's name is "bitcrush~" */
+    /* create a new class */
+    bitcrush_tilde_class = class_new(gensym("bitcrush~"),            /* the object's name is "bitcrush~" */
                                (t_newmethod)bitcrush_tilde_new,    /* the object's constructor is "helloworld_new()" */
                                (t_method)bitcrush_tilde_free,      /* special destructor */
                                sizeof(t_bitcrush_tilde),           /* the size of the data-space */
                                CLASS_DEFAULT,                      /* a normal pd object */
                                A_DEFFLOAT, 0);                     /* define input as a float */
 
-  class_addmethod(bitcrush_tilde_class,
+    class_addmethod(bitcrush_tilde_class,
                   (t_method)bitcrush_tilde_dsp,
                   gensym("dsp"),
                   A_CANT, 0);
+    CLASS_MAINSIGNALIN(bitcrush_tilde_class, t_bitcrush_tilde, f); // specify first inlet to be a signal by default
 }
